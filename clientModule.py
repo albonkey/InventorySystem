2998a5dbff7e521af06352b0f8858d1767f8da1d
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2020-12-08 23:03:08 -0800

    Add files via upload

--- clientModule.py
+++ clientModule.py
@@ -1,28 +1,59 @@
 from classes import Client
+import pymongo
+from pymongo import MongoClient
 
-class ClientModule():
+
+class ClientModule:
     def __init__(self):
-        self.clients = [
-            Client(1, "carl", "carl@gmail.com", "Los Angeles"),
-            Client(2, "alejandro", "alejandro@gmail.com", "Los Angeles"),
-            Client(3, "nicol", "nicol@gmail.com", "Los Angeles")
-        ]
+        # database connection
+        self.connection_string = "mongodb+srv://ultra:dundun428@ims.bnlzj.mongodb.net/inventory_MS?retryWrites=true&w" \
+                                 "=majority "
+        self.client = pymongo.MongoClient(self.connection_string)
+        self.db = self.client.inventory_MS
+        self.collection = self.db.customers
 
-    #Connect database here. And make the functions update the database
+    def get_all_clients(self):
+        all_clients = self.collection.find({})
+        return all_clients
+    # Create Clients (id, name, email, address)
+    def createClient(self, id, name, email, address):
+        """ This functions creates a customer object in the mondoDB database using pymongo."""
+        # Client document to be inserted into the DB
+        document = {
+            "Customer": {
+                "CustomerID": id,
+                "CustomerName": name,
+                "Email": email,
+                "Address": address,
+            }
+        }
+        # Creating a new Client document in the DB
+        self.collection.insert_one(document)
 
-    #Create Clients (id, name, email, address)
-    def createClient(self,id, name, email, address):
-        self.clients.append(Client(id, name, email, address))
-
-
-    def getClients(self):
-        return self.clients
+    def getClient(self, email):
+        """ This function returns a customer dictionary object from the DB using their email"""
+        # Client is a dictionary object
+        query = {"Customer.Email": email}
+        client = self.collection.find_one(query)
+        return client
 
     def updateClient(self, id, name, email, address):
-        return null
+        """ This function updates a customers info on the DB. """
+        query = {"Customer.CustomerID": id}
+        new_values = {"$set":
+                            {"Customer":
+                                {
+                                    "CustomerID": id,
+                                    "CustomerName": name,
+                                    "Email": email,
+                                    "Address": address}
+                            }}
+        self.collection.update_one(query, new_values)
 
-    def removieClient(self, client_id):
-        return nulll
+    def removeClient(self, client_id):
+        """ This function removes one customer from the DB. """
+        query = {"Customer.CustomerID": client_id}
+        self.collection.delete_one(query)
 
 
 clientModule = ClientModule()
--- clientsView.py
+++ clientsView.py
@@ -1,35 +1,38 @@
 import tkinter as tk
+import uuid
 from clientModule import clientModule
 from sidebar import Sidebar
+
 
 class ClientsView(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.heading = tk.Label(self, text="Clients", background="lightgrey",pady="5")
+        self.heading = tk.Label(self, text="Clients", background="lightgrey", pady="5")
         self.heading.pack(side="top", fill="x")
         self.sidebar = Sidebar(self, "Clients")
         self.sidebar.pack(side="left", fill="y")
         self.main = tk.Frame(master=self)
         self.clientList = tk.Frame(master=self.main, padx=30, pady=30)
-        self.clientListInit()
+        #self.clientListInit()
         self.clientAdd = tk.Frame(master=self.main, padx=30, pady=30)
         self.clientAddInit()
         self.switchMain("Client List")
-
+    """
     def clientListInit(self):
         rowcount = 1
-        for client in clientModule.getClients():
+        
+     for client in clientModule.getClients():
             colcount = 0
             for attribute, value in client.__dict__.items():
-                if(rowcount == 1):
+                if (rowcount == 1):
                     self.label = tk.Label(master=self.clientList, text=attribute)
-                    self.label.grid(row=rowcount-1, column=colcount)
+                    self.label.grid(row=rowcount - 1, column=colcount)
                 self.label = tk.Label(master=self.clientList, text=value)
-                self.label.grid(row=rowcount+1, column=colcount)
+                self.label.grid(row=rowcount + 1, column=colcount)
                 colcount += 1
             rowcount += 1
-
+        """
     def clientAddInit(self):
         lbl_name = tk.Label(master=self.clientAdd, text="Name")
         ent_name = tk.Entry(master=self.clientAdd)
@@ -38,8 +41,8 @@ class ClientsView(tk.Frame):
         lbl_address = tk.Label(master=self.clientAdd, text="Address")
         ent_address = tk.Entry(master=self.clientAdd)
         btn_submit = tk.Button(master=self.clientAdd, text="Add Client", height=3, width=10,
-            command= lambda: self.createClient(ent_name.get(), ent_email.get(), ent_address.get())
-        )
+                                command=lambda: self.createClient(ent_name.get(), ent_email.get(), ent_address.get())
+                               )
         lbl_name.pack()
         ent_name.pack()
         lbl_email.pack()
@@ -48,18 +51,19 @@ class ClientsView(tk.Frame):
         ent_address.pack()
         btn_submit.pack()
 
-
     def createClient(self, name, email, address):
-        clientModule.createClient(1, name, email, address)
-        self.clientListInit()
+        # Added uuid to create a unique ID in HEX
+        client_id = uuid.uuid1()
+        clientModule.createClient(client_id.hex, name, email, address)
+        #self.clientListInit() temporarly disabled
         self.switchMain("Client List")
 
     def switchMain(self, name):
         self.clientList.pack_forget()
         self.clientAdd.pack_forget()
-        if(name == "Client List"):
+        if name == "Client List":
             self.clientList.pack()
-        elif(name == "Add Client"):
+        elif name == "Add Client":
             self.clientAdd.pack()
 
         self.main.pack()
