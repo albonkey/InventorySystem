2c4287b34a4baa7459c5238cf9e8dd356acdfe55
albonkey <43227255+albonkey@users.noreply.github.com>
2020-12-09 22:44:40 -0800

    Commit

--- controller.py
+++ controller.py
@@ -4,14 +4,22 @@ from sidebar import Sidebar
 from clientsView import ClientsView
 from invoicesView import InvoicesView
 from productsView import ProductsView
+import tkinter.font as tkFont
 
 class UserInterface(tk.Frame):
+
+
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.header = Header(self)
         self.header.pack(side="top", fill="both")
-        self.view = ClientsView(self)
+        self.view = tk.Frame(master=self)
+        fontStyle = tkFont.Font(family="Lucida Grande", size=20)
+        self.welcome = tk.Label(master=self.view, text="WELCOME", pady=10, font=fontStyle, bg="lightgrey")
+        self.welcome.pack(fill="x", expand=True)
+        self.underHeading = tk.Label(master=self.view, text="Press the menu to get started", pady=30)
+        self.underHeading.pack()
         self.view.pack(fill="both")
 
 
